ce9b860e17f4aec7923fba2bf37e0e7b9e63bd5f
abhinavdogra21 <dograabhinav21@gmail.com>
2025-06-05 01:22:46 +0530

    dsfl

---
+++ Number Theory /powerset.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+class Solution {
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        int n=nums.size();
+        vector<vector<int>> ans;
+        int ns= (1<<n)-1;
+        for(int i=0;i<=ns;i++)
+        {
+              vector<int> list;
+            for(int j=0;j<n;j++)
+            {
+                if(i&(1<<j)) list.push_back(nums[j]);
+            }
+        ans.push_back(list);
+        }
+        return ans;
+        
+    }
+};
